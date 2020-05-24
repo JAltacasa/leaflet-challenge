69e3425d07257e05bb9dc43685f9d4934630d4c5
unknown <jma.hogewoning@gmail.com>
2020-05-23 19:30:03 -0500

    Final

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,4 +1,4 @@
-// Store API link
+// Store the data link
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 function markerSize(mag) {
@@ -21,17 +21,17 @@ function markerColor(mag) {
   };
 }
 
-// Perform a GET request to the query URL
+// GET request to the query URL
 d3.json(link, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
+  // Sending the data.features object to the createFeatures function
   createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
   var earthquakes = L.geoJSON(earthquakeData, {
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
+  // Defining a function we want to run once for each feature in the features array
+  // Giving each feature a popup describing the place and time of the earthquake
  onEachFeature : function (feature, layer) {
 
     layer.bindPopup("<h3>" + feature.properties.place +
@@ -52,7 +52,7 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
 
-  // Define satelitemap and darkmap layers
+  // Defining satelitemap and darkmap layers
   var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -67,27 +67,27 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Define a baseMaps object to hold our base layers
+  // Defining a baseMaps object to hold our base layers
   var baseMaps = {
     "Satelite Map": satelitemap,
     "Dark Map": darkmap
   };
 
-  // Create overlay object to hold our overlay layer
+  // Creating overlay object to hold our overlay layer
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the satelitemap and earthquakes layers to display on load
+  // Creating our map, giving it the satelitemap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [31.57853542647338,-99.580078125],
     zoom: 3,
     layers: [satelitemap, earthquakes]
   });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  // Creating a layer control
+  // Passing in our baseMaps and overlayMaps
+  // Adding the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
